0000c20d52e5756cdaf2eda24c0b551bb861574c
jato <jatobrown@gmail.com>
2016-03-10 13:58:08 -0800

    cleans up models

--- update.sh
+++ update.sh
@@ -2,4 +2,5 @@
 . ~/.profile
 ebooks archive jato corpus/jato.json  
 ebooks archive kevinrose corpus/kevinrose.json  
-ebooks consume-all combined corpus/jato.json corpus/kevinrose.json
+ebooks archive summertomato corpus/summertomato.json  
+ebooks consume-all combined corpus/jato.json corpus/kevinrose.json corpus/summertomato.json
