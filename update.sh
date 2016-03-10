3e954572beec697612cb8a7475c569d7544b61b4
jato <jatobrown@gmail.com>
2016-03-10 14:39:28 -0800

    Updates combined model

--- update.sh
+++ update.sh
@@ -2,5 +2,8 @@
 . ~/.profile
 ebooks archive jato corpus/jato.json  
 ebooks archive kevinrose corpus/kevinrose.json  
+ebooks archive damienfahey corpus/damienfahey.json  
 ebooks archive summertomato corpus/summertomato.json  
-ebooks consume-all combined corpus/jato.json corpus/kevinrose.json corpus/summertomato.json
+ebooks consume-all combined corpus/jato.json corpus/kevinrose.json corpus/summertomato.json corpus/damienfahey.json
+
+# type "./update.sh" in Terminal to run the update script and grab new tweets 
